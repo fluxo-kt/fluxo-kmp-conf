d59d9a5b27374e7070aca7be632bb0d4125542f0
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-10-15 18:14:27 +0200

    test: init main test module
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

---
+++ checks/main/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    alias(libs.plugins.deps.guard)
+}
+
+dependencyGuard {
+    configuration("classpath")
+}
+
+if (hasProperty("buildScan")) {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
