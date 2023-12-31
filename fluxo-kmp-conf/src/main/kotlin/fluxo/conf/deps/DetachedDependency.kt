1f9816d0433dac0dafccf5118c00cd040016bc08
Artyom Shendrik <artyom.shendrik@gmail.com>
2023-12-31 18:34:28 +0100

    feat: add util methods for detached dependencies in Gradle
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

---
+++ fluxo-kmp-conf/src/main/kotlin/fluxo/conf/deps/DetachedDependency.kt
@@ -0,0 +1,25 @@
+package fluxo.conf.deps
+
+import fluxo.util.mapToArray
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+
+internal fun Project.detachedDependency(
+    groupId: String,
+    artifactId: String,
+    version: String,
+): Configuration = detachedDependency("$groupId:$artifactId:$version")
+
+internal fun Project.detachedDependency(dependencyNotation: Any): Configuration =
+    configurations.detachedConfiguration(
+        dependencies.create(dependencyNotation),
+    )
+
+internal fun Project.detachedDependency(vararg dependencyNotation: Any): Configuration =
+    detachedDependency(dependencyNotation)
+
+@JvmName("detachedDependencyArray")
+internal fun Project.detachedDependency(dependencyNotations: Array<out Any>): Configuration =
+    configurations.detachedConfiguration(
+        *dependencyNotations.mapToArray { dependencies.create(it) },
+    )
