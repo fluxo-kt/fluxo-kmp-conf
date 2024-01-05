bddb8cd97c541e6f75b7c2ac4b355701fc8b39b6
Artyom Shendrik <artyom.shendrik@gmail.com>
2024-01-05 20:43:15 +0100

    feat: simplify logging, remove custom log markers completely
    Signed-off-by: Artyom Shendrik <artyom.shendrik@gmail.com>

--- fluxo-kmp-conf/src/main/kotlin/fluxo/conf/impl/LoggerUtils.kt
+++ fluxo-kmp-conf/src/main/kotlin/fluxo/conf/impl/LoggerUtils.kt
@@ -6,86 +6,65 @@ import org.gradle.api.logging.Logger
 
 
 private const val TAG = "FluxoKmpConf"
-private const val LOG_TASK_PREFIX = "> "
-private const val CONF = LOG_TASK_PREFIX + "Conf"
-private const val FLUXO = "Fluxo"
 
 @Volatile
 internal var SHOW_DEBUG_LOGS = false
 
 
-internal fun Logger.t(message: String) {
-    if (SHOW_DEBUG_LOGS) {
-        lifecycle("{}Fv: {}", LOG_TASK_PREFIX, message)
-    } else {
-        trace("{}{} {}", LOG_TASK_PREFIX, TAG, message)
-    }
+internal fun Logger.t(message: String) = when {
+    !SHOW_DEBUG_LOGS -> trace("{} {}", TAG, message)
+    else -> lifecycle("v: {}", message)
 }
 
-internal fun Logger.t(message: String, e: Throwable?) {
-    if (SHOW_DEBUG_LOGS) {
-        lifecycle("${LOG_TASK_PREFIX}Fv: $message", e)
-    } else {
-        trace("$LOG_TASK_PREFIX$TAG: $message", e)
-    }
+internal fun Logger.t(message: String, e: Throwable?) = when {
+    !SHOW_DEBUG_LOGS -> trace("$TAG: $message", e)
+    else -> lifecycle("v: $message", e)
 }
 
-internal fun Logger.v(message: String, e: Throwable? = null) = t(message, e)
+internal fun Logger.v(message: String) = t(message)
+
+internal fun Logger.v(message: String, e: Throwable?) = t(message, e)
 
 
-internal fun Logger.d(message: String, e: Throwable? = null) {
-    if (SHOW_DEBUG_LOGS) {
-        lifecycle("${LOG_TASK_PREFIX}F: $message", e)
-    } else {
-        debug("$LOG_TASK_PREFIX$TAG: $message", e)
-    }
+internal fun Logger.d(message: String, e: Throwable? = null) = when {
+    !SHOW_DEBUG_LOGS -> debug("$TAG: $message", e)
+    else -> lifecycle("d: $message", e)
 }
 
-internal fun Logger.d(message: String, arg1: Any?, arg2: Any?) {
-    if (SHOW_DEBUG_LOGS) {
-        lifecycle("{}F: $message", LOG_TASK_PREFIX, arg1, arg2)
-    } else {
-        debug("{}$TAG: $message", LOG_TASK_PREFIX, arg1, arg2)
-    }
+internal fun Logger.d(message: String, arg1: Any?, arg2: Any?) = when {
+    !SHOW_DEBUG_LOGS -> debug("$TAG: $message", arg1, arg2)
+    else -> lifecycle("d: $message", arg1, arg2)
 }
 
-internal fun Logger.d(message: String, arg1: Any?, arg2: Any?, arg3: Any?) {
-    if (SHOW_DEBUG_LOGS) {
-        lifecycle("{}F: $message", LOG_TASK_PREFIX, arg1, arg2, arg3)
-    } else {
-        debug("{}$TAG: $message", LOG_TASK_PREFIX, arg1, arg2, arg3)
-    }
+internal fun Logger.d(message: String, arg1: Any?, arg2: Any?, arg3: Any?) = when {
+    !SHOW_DEBUG_LOGS -> debug("$TAG: $message", arg1, arg2, arg3)
+    else -> lifecycle("d: $message", arg1, arg2, arg3)
 }
 
 
-internal fun Logger.i(message: String) {
-    if (SHOW_DEBUG_LOGS) {
-        lifecycle("${LOG_TASK_PREFIX}Fi: {}", message)
-    } else {
-        info("{} {}", CONF, message)
-    }
+internal fun Logger.i(message: String) = when {
+    !SHOW_DEBUG_LOGS -> debug(message)
+    else -> lifecycle("i: {}", message)
 }
 
-internal fun Logger.i(message: String, arg1: Any?) {
-    if (SHOW_DEBUG_LOGS) {
-        lifecycle("{}Fi: $message", LOG_TASK_PREFIX, arg1)
-    } else {
-        info("{} $message", CONF, arg1)
-    }
+internal fun Logger.i(message: String, arg1: Any?) = when {
+    !SHOW_DEBUG_LOGS -> debug(message, arg1)
+    else -> lifecycle("i: $message", arg1)
 }
 
 
-internal fun Logger.l(message: String) = lifecycle("{} {}", CONF, message)
-internal fun Logger.l(message: String, vararg args: Any) = lifecycle("$CONF $message", *args)
+internal fun Logger.l(message: String) = lifecycle(message)
+internal fun Logger.l(message: String, vararg args: Any) = lifecycle(message, *args)
 
 
-internal fun Logger.w(message: String, e: Throwable? = null) = warn("w: $CONF! $message", e)
+internal fun Logger.w(message: String) = warn("w: {}", message)
+internal fun Logger.w(message: String, e: Throwable?) = warn("w: $message", e)
 
 
-internal fun Logger.e(message: String) = error("e: {} {}", FLUXO, message)
+internal fun Logger.e(message: String) = error("e: {}", message)
 
-internal fun Logger.e(message: String, e: Throwable?) = error("e: $FLUXO $message", e)
+internal fun Logger.e(message: String, e: Throwable?) = error("e: $message", e)
 
-internal fun Logger.e(message: String, arg1: Any?) = error("e : {} $message", FLUXO, arg1)
+internal fun Logger.e(message: String, arg1: Any?) = error("e: $message", arg1)
 
-internal fun Logger.e(message: String, vararg args: Any?) = error("e: $FLUXO $message", *args)
+internal fun Logger.e(message: String, vararg args: Any?) = error("e: $message", *args)
